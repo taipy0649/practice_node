c615c294f1634324c0558c2d874bb96ff9bd4c0c
taiyo <nishimototai@gmail.com>
2022-07-02 22:22:07 +0900

    lesson10, add partial to layout.ejs

--- template_lesson10/main.js
+++ template_lesson10/main.js
@@ -1,10 +1,13 @@
 const port = 3000,
     express = require('express'),
     app = express(),
-    homeController = require('./controllers/homeController')
+    homeController = require('./controllers/homeController'),
+    layout = require('express-ejs-layouts')
 
 app.set('view engine', 'ejs')
     
+app.use(layout)
+
 app.use(
     express.urlencoded({
         extended: false
