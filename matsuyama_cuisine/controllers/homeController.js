90f91a9f6a0db8b93493b47cfb8ecd599a6560f6
taiyo <nishimototai@gmail.com>
2022-07-09 14:39:16 +0900

    give info about courses to views of courses

--- matsuyama_cuisine/controllers/homeController.js
+++ matsuyama_cuisine/controllers/homeController.js
@@ -1,5 +1,19 @@
+var courses = [
+    {
+        title: 'Event Driven Cakes',
+        cost: '50'
+    },
+    {
+        title: 'sample course',
+        cost: 100
+    }
+]
+
 exports.showCourses = (req, res) => {
-    res.render('courses')
+    res.render('courses', {
+        // コースの配列をビューに渡す
+        offeredCourses: courses
+    })
 }
 
 exports.showSingUp = (req, res) => {
